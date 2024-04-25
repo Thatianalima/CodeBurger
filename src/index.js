fad2d8c36bb1e92725015963e3866fb2471c0e7a
Thatianalima <taty_sumare@hotmail.com>
2024-04-25 12:05:02 -0300

    error

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Login from "./containers/login";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Login />);
